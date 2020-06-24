665aa3a1a2a2122aa61ee0806dbb1ddbd8d6ba31
Pi <paingthetko.pi@gmail.com>
2020-06-24 19:57:03 +0800

    css

--- js/app.js
+++ js/app.js
@@ -9,7 +9,18 @@ This is the main JavaScript file for all the functions running on the web page. 
 $(document).ready(function () {
 
 	$('#loginbutton').click(function () {
-		alert('yes');
+		$(".component-loginbox").addClass("active");
+		//hides navbar for a while
+		$(".navbar").hide();
 	});
+	
+	
+	//Function to close the login box and 
+	$('#loginbox-close').click(function () {
+		$(".component-loginbox").removeClass("active");
+		$(".navbar").show();
+	});
+	
+	
 
 });
