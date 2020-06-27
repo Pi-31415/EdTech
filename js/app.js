c68083ba909165c35b03d15ae1c810b6d82b5e20
Pi <paingthetko.pi@gmail.com>
2020-06-27 10:57:31 +0800

    carousel

--- js/app.js
+++ js/app.js
@@ -17,11 +17,11 @@ function loginboxclose() {
 }
 
 //Venn Diagram relate functions
-function vennhighlight(vennid){
+function vennhighlight(vennid) {
 	//clear all selected
 	$("path").removeClass();
 	//select when mouse hovers
-	$("#"+vennid).addClass("activated");
+	$("#" + vennid).addClass("activated");
 }
 
 //These are pseudo functions to demonstrate login and schedule
@@ -99,35 +99,35 @@ function schedule() {
 		$('#schedule-form-errors').html("Please enter a valid email address.");
 		$('#schedule-form-errors').fadeIn();
 	}
-	
+
 	if (phone.length > 0) {
 		checklist[3] = true;
 	} else {
 		$('#schedule-form-errors').html("Please enter phone number.");
 		$('#schedule-form-errors').fadeIn();
 	}
-	
+
 	if (/^\d+$/.test(phone)) {
 		checklist[4] = true;
 	} else {
 		$('#schedule-form-errors').html("Please enter a valid phone number.");
 		$('#schedule-form-errors').fadeIn();
 	}
-	
+
 	if (email.length > 0) {
 		checklist[0] = true;
 	} else {
 		$('#schedule-form-errors').html("Please fill in your email.");
 		$('#schedule-form-errors').fadeIn();
 	}
-	
+
 	if (name.length > 0) {
 		checklist[1] = true;
 	} else {
 		$('#schedule-form-errors').html("Please enter your full name.");
 		$('#schedule-form-errors').fadeIn();
 	}
-	
+
 	if (checklist[0] == true && checklist[1] == true && checklist[2] == true && checklist[3] == true && checklist[4] == true) {
 		//clear the form fields
 		$("#name").val("");
@@ -135,21 +135,48 @@ function schedule() {
 		$("#phone").val("");
 		//create a temporary login effect
 		var contact_message = "Thank you very much. We will be contacting you soon.";
-		$('#schedule-form-success').html("<br>"+contact_message);
+		$('#schedule-form-success').html("<br>" + contact_message);
 		$('#schedule-form-success').fadeIn();
-	}
-	else{
+	} else {
 		jumpto('#component-freetrial');
 	}
-	
+
 }
 
 //Main function which runs when the page loads
 $(document).ready(function () {
+	//Venn Diagram auto rotation code
+	
+	vennhighlight('venn-academics');
+	var venn_index = 0;
+	var venn_interval_time = 500;
+	var venn_id = ['venn-academics', 'venn-interest', 'venn-portfolio'];
+	var venn_auto_rotate = setInterval(function () {
+		if (venn_index == venn_id.length) {
+			venn_index = 0;
+		}
+		vennhighlight(venn_id[venn_index]);
+		venn_index++;
+	}, venn_interval_time);
+
+	//venn diagram rotate pause when mouse in
+	$(".venn-btn").mouseover(function () {
+		clearInterval(venn_auto_rotate);
+	});
+	$(".venn-btn").mouseout(function () {
+		venn_auto_rotate = setInterval(function () {
+			if (venn_index == venn_id.length) {
+				venn_index = 0;
+			}
+			vennhighlight(venn_id[venn_index]);
+			venn_index++;
+		}, venn_interval_time);
+	});
+
 	//Hide unnecessary components (such as notices and validation messages) for fade in
 	$('#username_navbar').hide();
 	$('#schedule-form-success').hide();
-	
+
 	//Log in Popup
 	$('#loginbutton').click(function () {
 		$(".component-loginbox").addClass("active");
