70be54e0876a2863e6b33ef183e468ea8ebcdc96
Pi <paingthetko.pi@gmail.com>
2020-06-30 17:56:38 +0630

    Update temp_register.php

--- temp_register.php
+++ temp_register.php
@@ -1,7 +1,10 @@
 <?php
-$myfile = fopen("customer.txt", "w") or die("Unable to open file!");
+$myfile = fopen("customer.txt", "r") or die("Unable to open file!");
 
 $original_content =  fread($myfile,filesize("customer.txt"));
+
+$myfile = fopen("customer.txt", "w") or die("Unable to open file!");
+
 
 $txt = $original_content."John Doe\n";
 fwrite($myfile, $txt);
