883a490d393a80e39161fac7bfaea407da81a319
Pi <paingthetko.pi@gmail.com>
2020-07-01 10:02:43 +0630

    Update app.js

--- js/app.js
+++ js/app.js
@@ -352,7 +352,7 @@ $(document).ready(function () {
 		div.datum(sets).call(chart);
 		color_venn();
 		d3.selectAll("#venn text").style("fill", "#fff");
-		d3.selectAll("#venn text").style("font-size", "0.5em");
+		d3.selectAll("#venn text").style("font-size", "0.7em");
 	} else if (window.innerWidth > 650 && window.innerWidth < 1175) {
 		//phones
 		var chart = venn.VennDiagram().width(w / 1.2).height(w / 1.2);
